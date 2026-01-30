be1137cca4e34a18a4f9163016ee0ead72240662
viorel <viorel@home.com>
2026-01-30 08:50:07 +0100

    x

--- src/App.jsx
+++ src/App.jsx
@@ -6,55 +6,15 @@ import { useRef, useEffect, useLayoutEffect, useMemo } from 'react'
 
 function App() {
 
-  const camOptions = useMemo(() => {
-    return {
-      x: { value: 13, min: 0, max: 50, step: 1 },
-      y: { value: 6, min: 0, max: 50, step: 1 },
-      z: { value: 10, min: 0, max: 50, step: 1 },
-      visible: true,
-      color: { value: 'lime' }
-    }
-  }, [])
-
-  const avaOptions = useMemo(() => {
-    return {
-      x: { value: 0, min: 0, max: 10, step: 0.1 },
-      y: { value: 0, min: 0, max: 10, step: 0.1 },
-      z: { value: 0, min: 0, max: 10, step: 0.1 },
-      visible: true,
-      color: { value: 'lime' }
-    }
-  }, [])
-
-  const animOptions = useMemo(() => {
-    return {
-      anim: { value: 'bencao', options: ['bencao', 'armada'] }
-    }
-  }, [])
-
-  
-
-  const { cam } = useRef(null)
-
-  const pC = useControls('Camera', camOptions)
-  const pV = useControls('Viorel', avaOptions)
-  const animation = useControls('animation', animOptions)
-
-  useEffect(() => {
-    console.log(cam)
-    //cam.lookAt(0, 2, 0)
-  }, [])
-
   return (
     <>
-      <Leva />
       <Canvas shadows >
         <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
         <Environment preset="sunset"/>
-        <PerspectiveCamera makeDefault position={[pC.x, pC.y, pC.z]} />
+        <PerspectiveCamera makeDefault position={[1, 1, 10]} />
         <OrbitControls />
         <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
-        <F1Car position={[pV.x, 0.55, pV.z]}/> 
+        <F1Car position={[0, 0.55, 0]}/> 
         <mesh receiveShadow scale={50} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
           <planeGeometry/>
           <meshStandardMaterial color={"white"}/>
